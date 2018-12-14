f3b65155af471596cd999b725cc6c3ff88d545a9
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-14 21:05:10 +0530

    Create string2.py
    All string manipulations

---
+++ string2.py
@@ -0,0 +1,24 @@
+jyo = "Hello TACT"
+print("Prints everything in upper case")
+print(jyo.upper())
+print("Prints everything in lower case")
+print(jyo.lower())
+print("Capitalizes the begining  of each word:")
+print(jyo.title())
+print("Capitalizes all the elements:")
+print(jyo.capitalize())
+print("Swaps upper to lower:")
+print(jyo.swapcase())
+print("Returns true if the string is upper:")
+print(jyo.isupper())
+print("Returns true if the string is lower:")
+print (jyo.islower())
+print("Splits the string with comma:")
+print(jyo.split())
+print("Splits the string :")
+print(jyo.split(','))
+print(jyo.isalnum())         #prints true if all char are alphanumeric else false
+print(jyo.isalpha())        #prints true if all char in the string are alphabetic else false
+print(jyo.isdigit())         #prints true if string contains digits else false
+print(jyo.istitle())        #prints true if string contains title words else false
+print(jyo.isspace())        #prints true if string contains spaces else false
