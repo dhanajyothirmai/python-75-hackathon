3f9dcb73c6eb506fc5461e202c697f4990d1fc4b
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-14 22:06:29 +0530

    Create string3.py

---
+++ string3.py
@@ -0,0 +1,11 @@
+a = "Dhana"
+b = "Jyothirmai"
+c = "18"
+
+print("{} {} is {} years old".format(a,b,c))
+
+print(' '.join(['do', 'more', 'of', 'what', 'matters', 'to', 'you']))
+
+dhana = ["a","b","c"]
+         
+print(' '.join(dhana))
