c9c209f73a5fda22d1ebf5513746ef0d453426ec
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-14 19:42:00 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,16 @@
+jyo = "Hello TACT"
+print("Initial String:")
+print(jyo)
+jyo = "Welcome to TACT"
+print("\nUpdated string:")
+print(jyo)
+print("\nCharacters from 3-5:")
+print ("jyo[2:5]:", jyo[2:5])
+print("\n Characters between "+"3rd and 2nd last character")
+print ("jyo[3:-2]:",jyo[3:-2])
+print("\nPrinting string 4 times")
+print (jyo*4)
+print("\nAdding element to the string:")
+print(jyo +"TEAM")
+print("\nFinding the index of the element: ")
+"Hello tact TEAM".index("E")
