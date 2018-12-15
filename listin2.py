2ef2f001d190f881bd7f26579835e905a82b7aac
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-15 07:56:03 +0530

    Create listin2.py

---
+++ listin2.py
@@ -0,0 +1,12 @@
+kasturi = ["this","is","a","woman","of","love"]
+
+jyo = [ word[0] for word in kasturi ]
+
+print(jyo)
+print([jyo1.lower() for jyo1 in ["A","B","C"]])
+print([jyo1.upper() for jyo1 in ["a","b","c"]])
+
+y = "Hello 8744 Tact"
+hey = [x for x in y  if x.isdigit()]
+print(hey)
+
