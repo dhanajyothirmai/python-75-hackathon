9c4248a1199deb60c4648364b0890cdc0761bc47
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-15 11:20:08 +0530

    Create tupple.py

---
+++ tupple.py
@@ -0,0 +1,10 @@
+jyo1 = ('physics', 'chemistry', 1997, 2000);
+jyo2 = (1, 2, 3, 4, 5, 6, 7 );
+print("jyo1[0]: ", jyo1[0])
+print("jyo2[1:5]: ", jyo2[1:5])
+jyo1 = (15, 37.66);
+jyo2 = ('ggtd', 'vfdy');
+
+
+jyo3 = jyo1 + jyo2;
+print(jyo3)
