94991c9d0611d53bbc707c27a62634c510b7c61e
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-15 07:49:17 +0530

    Create listinfunction.py

---
+++ listinfunction.py
@@ -0,0 +1,17 @@
+L = ['ram','sita','dev','mira','lax']
+if 'ram' in L:
+ print("list contains", 'ram')
+for sita in L:
+ print(sita)
+
+jyo = []
+
+for x in range(10):
+    jyo.append(x**2)
+ 
+print(jyo)
+print("another method")
+
+jyo1 = [x**2 for x in range(10)]
+
+print(jyo1)
