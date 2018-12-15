5a71d18bba20c592dd9ae5137125defa304251a4
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-15 07:19:44 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+L = ['jyo', 'dhana', 'sri','durga']
+
+print(L)
+print("prints list from 2 to 3")
+print(L[1:3])
+print("prints list after 2")
+print(L[2:]) 
+print("prints list before 2")
+print(L[:2]) 
+print("prints list from before last one")
+print(L[-1]) 
+print("prints list eliminating first and last")
+print(L[1:-1])
