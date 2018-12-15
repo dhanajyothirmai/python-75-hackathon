1b0021f6c222218dc6f408e5e24598111125953e
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-15 12:24:46 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,6 @@
+dict = {'Name': 'jyo', 'Age': 17, 'Class': 'grad'}
+dict['Age'] = 19 ; #  here we updated existing entry
+dict['School'] = "Ghs School"; #  here we added new entry
+
+print( "dict['Age']: ", dict['Age'])
+print ("dict['School']: ", dict['School'])
