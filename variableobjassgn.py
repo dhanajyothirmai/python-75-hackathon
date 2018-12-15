5bd1275d3d05e0c147729c93cff31f866f3750a9
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-15 08:41:49 +0530

    Create variableobjassgn.py

---
+++ variableobjassgn.py
@@ -0,0 +1,17 @@
+ jyo = 30 
+    
+jyo2 = jyo
+
+print("object identity of jyo")
+
+print(id(jyo))
+
+print("object identity of jyo2 before assignment")
+
+print(id(jyo2))
+
+jyo2 = 400
+
+print("object identity of jyo2 after assignment")
+
+print(id(jyo2))
