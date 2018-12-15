40a502d0ef81a7b1462e02d97b30b9a3445dbd2c
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-15 11:58:38 +0530

    Create tupdelete.py

---
+++ tupdelete.py
@@ -0,0 +1,11 @@
+jyo1 = ('phy', 'chem', 2001, 2019)
+jyo2 = ('tgf')
+print(jyo1)
+print(jyo2)
+del(jyo1)
+print("After deleting  : ")
+print(jyo1)
+#without brackets 
+print ('abc', -4.24e93, 18+6.6j, 'xyz')
+x, y = 1, 2;
+print ("Value of x , y : ", x,y)
