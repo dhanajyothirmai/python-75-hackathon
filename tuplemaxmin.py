c5d40aae5361ee1d559ba584284ffde010a4db4a
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-15 11:43:18 +0530

    Create tuplemaxmin.py

---
+++ tuplemaxmin.py
@@ -0,0 +1,6 @@
+jyo1, jyo2 = ('234','dhana', 'sri'), ('567', '675','caserd')
+print ("max length of jyoi1: ", max(jyo1))
+print ("max length  of jyo2: ", max(jyo2))
+
+print ("min length of jyo1: ", min(jyo1))
+print ("min length of jyo2 : ", min (jyo2))
