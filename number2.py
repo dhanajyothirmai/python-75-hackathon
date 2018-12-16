dbf832e6968f97143abf607afc6de1355fd624d8
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-16 07:22:07 +0530

    Create number2.py

---
+++ number2.py
@@ -0,0 +1,14 @@
+import math
+print  (math.exp(-45.17))
+print  (math.exp(100.12))
+print  (math.exp(100.72))
+ 
+print  (math.exp(math.pi))
+print  (math.floor(-45.17))
+print  (math.floor(100.12))
+print (math.floor(100.72))
+
+
+
+print (math.log(math.pi))
+print  (math.log10(100.12))
