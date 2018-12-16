e1b8c59e951626f2516e7895ca94666915b1e0a9
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-16 07:31:38 +0530

    Create number3.py

---
+++ number3.py
@@ -0,0 +1,15 @@
+import math
+       
+print ( math.modf(100.12))
+print  (math.modf(100.72))
+
+print  (round(80.23456, 2))
+print (round(100.000056, 3))
+print  (round(-100.000056, 3))
+print  (math.sqrt(100))
+print ( math.sqrt(7))
+
+
+print  (math.pow(100, -2))
+print (math.pow(2, 4))
+print (math.pow(3, 0))
