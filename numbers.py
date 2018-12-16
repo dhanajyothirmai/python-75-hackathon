e2623a121c2dae393cee6b253eb5ddf24bac9f4e
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-16 07:16:08 +0530

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,13 @@
+print (abs(-45))
+print (abs(100.12))
+print (abs(119))
+import math
+print (math.fabs(100.12))
+print ( math.fabs(100.72))
+print ( math.fabs(119))
+print( math.ceil(-45.17))
+print (math.ceil(-100.12))
+print( math.ceil(45.17))
+print (math.ceil(100.12))
+print(math.pi)
+print ( math.ceil(math.pi))
