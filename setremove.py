df167e1e7fb97b4acf6db573bdd75a21dc9425c5
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-16 10:02:43 +0530

    Create setremove.py

---
+++ setremove.py
@@ -0,0 +1,25 @@
+jyo = {"apple", "banana", "cherry"}
+
+print(len(jyo))
+jyo = {"apple", "banana", "cherry"}
+
+jyo.remove("banana")
+
+print(jyo)
+jyo = {"apple", "banana", "cherry"}
+
+jyo.discard("banana")
+
+print(jyo)
+jyo = {"apple", "banana", "cherry"}
+
+x = jyo.pop()
+
+print(x)
+
+print(jyo)
+jyo = {"apple", "banana", "cherry"}
+
+jyo.clear()
+
+print(jyo)
