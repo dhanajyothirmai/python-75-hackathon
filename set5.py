9c2d02c1f5d9375c7ffdd11beb78ce54eb3f9871
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-16 10:28:39 +0530

    Create set5.py

---
+++ set5.py
@@ -0,0 +1,20 @@
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.difference_update(y)
+print(x)
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.intersection_update(y) 
+print(x)
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.symmetric_difference(y) 
+print(x)
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.symmetric_difference_update(y)
+print(x)
