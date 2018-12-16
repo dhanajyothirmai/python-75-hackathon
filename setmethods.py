08a118431bfcf672da83e15a8ee716803cb5585b
dhanajyothirmai <43008285+dhanajyothirmai@users.noreply.github.com>
2018-12-16 10:11:57 +0530

    Create setmethods.py

---
+++ setmethods.py
@@ -0,0 +1,14 @@
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.intersection(y) 
+t = x.isdisjoint(y)
+g = x.union(y) 
+
+print(z)
+print(t)
+print(g)
+v = x.issubset(y) 
+f = x.issuperset(y) 
+print(v)
+print(f)
